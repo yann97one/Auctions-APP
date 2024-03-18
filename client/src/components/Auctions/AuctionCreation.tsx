fe1a046554be4b5c25ffc43c7f504486ad68f1d4
sdemolon2023 <sacha.demolon@hotmail.com>
2024-03-18 10:53:09 +0100

    feature: WIP js to jsx

--- client/src/components/Auctions/AuctionCreation.tsx
+++ client/src/components/Auctions/AuctionCreation.tsx
@@ -1,12 +1,96 @@
 import Datepicker from "../shared/Datepicker";
 import {useNavigate} from "react-router-dom";
 
+
+
+const uploadInput = document.getElementById('upload');
+const filenameLabel = document.getElementById('filename');
+const imagePreview = document.getElementById('image-preview');
+
+// Check if the event listener has been added before
+let isEventListenerAdded = false;
+
+uploadInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+        filenameLabel.textContent = file.name;
+
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            imagePreview.innerHTML =
+                `<img src="${e.target.result}" class="max-h-48 rounded-lg mx-auto" alt="Image preview" />`;
+            imagePreview.classList.remove('border-dashed', 'border-2', 'border-gray-400');
+
+            // Add event listener for image preview only once
+            if (!isEventListenerAdded) {
+                imagePreview.addEventListener('click', () => {
+                    uploadInput.click();
+                });
+
+                isEventListenerAdded = true;
+            }
+        };
+        reader.readAsDataURL(file);
+    } else {
+        filenameLabel.textContent = '';
+        imagePreview.innerHTML =
+            `<div class="bg-gray-200 h-48 rounded-lg flex items-center justify-center text-gray-500">No image preview</div>`;
+        imagePreview.classList.add('border-dashed', 'border-2', 'border-gray-400');
+
+        // Remove the event listener when there's no image
+        imagePreview.removeEventListener('click', () => {
+            uploadInput.click();
+        });
+
+        isEventListenerAdded = false;
+    }
+});
+
+uploadInput.addEventListener('click', (event) => {
+    event.stopPropagation();
+});
+
+
+
 function AuctionCreation() {
     const navigate = useNavigate();
 
     return (
         <div className="flex flex-row">
             <div className="flex flex-col w-1/3">
+                <div className="max-w-sm mx-auto bg-white rounded-lg shadow-md overflow-hidden items-center">
+                    <div className="px-4 py-6">
+                        <div id="image-preview"
+                             className="max-w-sm p-6 mb-4 bg-gray-100 border-dashed border-2 border-gray-400 rounded-lg items-center mx-auto text-center cursor-pointer">
+                            <input id="upload" type="file" className="hidden" accept="image/*"/>
+                            <label htmlFor="upload" className="cursor-pointer">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     stroke-width="1.5" stroke="currentColor"
+                                     className="w-8 h-8 text-gray-700 mx-auto mb-4">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"/>
+                                </svg>
+                                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-700">Upload picture</h5>
+                                <p className="font-normal text-sm text-gray-400 md:px-6">Choose photo size should be
+                                    less than <b className="text-gray-600">2mb</b></p>
+                                <p className="font-normal text-sm text-gray-400 md:px-6">and should be in <b
+                                    className="text-gray-600">JPG, PNG, or GIF</b> format.</p>
+                                <span id="filename" className="text-gray-500 bg-gray-200 z-50"></span>
+                            </label>
+                        </div>
+                        <div className="flex items-center justify-center">
+                            <div className="w-full">
+                                <label
+                                    className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 flex items-center justify-center mr-2 mb-2 cursor-pointer">
+                                    <span className="text-center ml-2">Upload</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
             <div className="flex flex-col w-96">
                 <form>
@@ -52,13 +136,13 @@ function AuctionCreation() {
                         Photo de l'article :
                     </label>
 
-                    <label htmlFor="file-input" className="sr-only">
+                    <label htmlFor="upload" className="sr-only">
                         Choose file
                     </label>
                     <input
                         type="file"
-                        name="file-input"
-                        id="file-input"
+                        name="upload"
+                        id="upload"
                         className="block max-w-sm w-full border border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600
     file:bg-gray-50 file:border-0 file:hover:bg-gray-100
     file:me-4
