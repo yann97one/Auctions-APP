feb886cc3544896391e1b3d1e8b4d49cd3155069
yann97one <yannissileber@gmail.com>
2024-03-20 11:16:11 +0100

    adding of aliases and navbar refactor

--- client/src/axios_helper.ts
+++ client/src/axios_helper.ts
@@ -1,39 +1,39 @@
 import axios from "axios";
-import { getTokenFromStorage } from "./services/localStorage";
+import {getTokenFromStorage} from "./services/localStorage";
 
 axios.defaults.baseURL = "http://localhost:8080/api";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 const token = getTokenFromStorage();
 
 if (token !== "undefined") {
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 }
 axios.interceptors.response.use(
-  (response) => {
-    if (response.headers["Set-Cookie"]) {
-      // Parcourez tous les cookies pour trouver celui qui commence par 'jwt='
-      const jwtCookie = response.headers["Set-Cookie"];
-      console.log(jwtCookie);
-      // Si un cookie JWT est trouvé
-      if (jwtCookie) {
-        // Extrait le token JWT du cookie
-        const jwtToken = jwtCookie.split(";")[0].split("=")[1];
+    (response) => {
 
-        // Stockez le token JWT dans le localStorage
-        localStorage.setItem("auth_token", jwtToken);
+        // Parcourez tous les cookies pour trouver celui qui commence par 'jwt='
+        const jwtCookie = response.headers["Set-Cookie"];
+        console.log(jwtCookie);
+        // Si un cookie JWT est trouvé
+        if (jwtCookie) {
+            // Extrait le token JWT du cookie
+            const jwtToken = jwtCookie.split(";")[0].split("=")[1];
 
-        // Configurez axios pour inclure le token JWT dans les futures requêtes
-        axios.defaults.headers.common["Authorization"] = `Bearer ${jwtToken}`;
-      }
+            // Stockez le token JWT dans le localStorage
+            localStorage.setItem("auth_token", jwtToken);
+
+            // Configurez axios pour inclure le token JWT dans les futures requêtes
+            axios.defaults.headers.common["Authorization"] = `Bearer ${jwtToken}`;
+        }
+
+
+        // Assurez-vous de renvoyer la réponse pour qu'elle puisse être traitée par la suite
+        return response;
+    },
+    (error) => {
+        // Gérez les erreurs de requête ici si nécessaire
+        return Promise.reject(error);
     }
-
-    // Assurez-vous de renvoyer la réponse pour qu'elle puisse être traitée par la suite
-    return response;
-  },
-  (error) => {
-    // Gérez les erreurs de requête ici si nécessaire
-    return Promise.reject(error);
-  }
 );
 
 // axios.interceptors.response.use(null, error => {
@@ -58,13 +58,13 @@ axios.interceptors.response.use(
 // })
 
 const request = (method: string, url: string, data?: any): Promise<any> => {
-  return axios({
-    method: method,
-    url: url,
-    data: data,
-  }).then((response) => response.data);
+    return axios({
+        method: method,
+        url: url,
+        data: data,
+    }).then((response) => response.data);
 };
 
 export default {
-  request,
+    request,
 };
--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,23 +1,22 @@
-import Filter from "../shared/Filter";
-import FilterLog from "../shared/FilterLog";
-import AuctionCard from "../shared/AuctionCard";
-import AuctionsList from "../auctions/AuctionsList";
+import Filter from "@components/shared/Filter";
+import FilterLog from "@components/shared/FilterLog";
+import AuctionsList from "@components/Auctions/AuctionsList";
 
 function Home() {
 
     return (
 
         <div className="flex flex-row">
-                <div className="flex flex-col  w-1/3">
-                    <div className="sticky top-12">
-                        <Filter/>
-                        <FilterLog/>
-                    </div>
-                </div>
-                <div className="flex flex-col w-2/3">
-                    <AuctionsList/>
+            <div className="flex flex-col  w-1/3">
+                <div className="sticky top-12">
+                    <Filter/>
+                    <FilterLog/>
                 </div>
             </div>
+            <div className="flex flex-col w-2/3">
+                <AuctionsList/>
+            </div>
+        </div>
     );
 }
 
