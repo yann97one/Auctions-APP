3cbb76b0e850846fc30daf856cbff1a68e3d485c
sdemolon2023 <sacha.demolon@hotmail.com>
2024-03-20 10:38:07 +0100

    feature: Viewprofile panel css

--- client/src/components/Profile/ViewProfile.tsx
+++ client/src/components/Profile/ViewProfile.tsx
@@ -54,40 +54,41 @@ function ViewProfile() {
   }, [isEditable]);
 
   return (
-    <div>
+
       <div>
-        {FORM_FIELDS.map((field, index) => (
-          <div key={index} className="flex flex-wrap -mx-3 mb-5">
-            <div className="md:w-1/2 px-3">
-              <label
-                htmlFor={field.label}
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                {field.label}
-              </label>
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-full lg:py-0">
+          <div
+              className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              {FORM_FIELDS.map((field, index) => (
+              <div key={index} className="flex flex-wrap -mx-3 mb-5">
+                <div className="md:w-1/2 px-2">
+                  <label htmlFor={field.label}
+                         className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    {field.label}</label>
+                </div>
+                <div className="md:w-1/2 px-2">
+                  <input type="text"
+                         id={field.label}
+                         name={field.name}
+                         value={user?.[field.id]}
+                         onChange={(event) =>
+                             setUser({ ...user, [field.id]: event.target.value })
+                         }
+                         disabled={!isEditable}
+                         className="block mb-2 text-sm font-medium text-gray-500 dark:text-white">
+                    </input>
+                </div>
+              </div>
+              ))}
+          <button onClick={updateUser}
+                  className="text-white mt-6 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  {isEditable ? "Valider" : "Modifier"}
+          </button>
             </div>
-            <div className="md:w-1/2 px-3">
-              <input
-                type="text"
-                id={field.label}
-                name={field.name}
-                value={user?.[field.id]}
-                onChange={(event) =>
-                  setUser({ ...user, [field.id]: event.target.value })
-                }
-                disabled={!isEditable}
-                className="block w-full px-4 py-2 mt-2 text-sm border rounded-lg focus:border-blue-500 focus:outline-none"
-              />
-            </div>
-          </div>
-        ))}
-      </div>
-      <button
-        onClick={updateUser}
-        className="text-white mt-6 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        {isEditable ? "Valider" : "Modifier"}
-      </button>
+        </div>
+    </div>
+
     </div>
   );
 }
